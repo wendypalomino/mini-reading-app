7701a4e2141406e98369d89a584624cf9be358ff
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-10-01 20:50:37 -0400

    tabb content

--- app.js
+++ app.js
@@ -76,3 +76,20 @@ function searchHandler(event){
 }
 
 searchBar.addEventListener('keyup', searchHandler)
+
+// Tabber content
+
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
